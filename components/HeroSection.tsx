b51bdc03d72386a4b7e10f5ab9debe69443c7405
eypae <e0559120@u.nus.edu>
2026-02-04 18:45:25 +0800

    first commit

---
+++ components/HeroSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import enyang_logo from '../../src/images/enyang-logo.png';
+
+const HeroSection = () => {
+    return (
+        <h1 className={"tracking-wide"}>Hi. I&apos;m En Yang.</h1>
+    );
+};
+export default HeroSection;
